7038a6494693e1eed9082c16a2e3066cf0b930db
RohithaReddy33 <rohithareddyanneda@gmail.com>
2025-01-28 20:34:39 +0530

    Add files via upload

---
+++ Operations.java
@@ -0,0 +1,10 @@
+public class Operations {
+    public static void main (String[]args){
+        int a = 90;
+        int b = 50;
+        System.out.printf("the addition of %d + %d is %d \n",a,b,a+b);
+        System.out.printf("the subtraction of %d - %d is %d \n",a,b,a-b);
+        System.out.printf("the multiplication of %d  mlt by %d is %d \n",a,b,a*b);
+        System.out.printf("the division of %d div %d is %d \n",a,b,a/b);
+    }
+}
